3bb7474d8a63e92095ded8b47e5e0ca78a491bfc
rbroderi <richard.broderick@gmail.com>
2026-09-11 19:47:59 -0400

    Preserve Lua error objects during unwind

--- src/luapyre/errors.py
+++ src/luapyre/errors.py
@@ -14,5 +14,17 @@ class LuaRuntimeError(LuaPyreError):
     pass
 
 
+class LuaRaisedError(LuaRuntimeError):
+    """A Lua-level error that retains its Lua error object."""
+
+    def __init__(self, value):
+        self.value = value
+        if isinstance(value, bytes):
+            message = value.decode("utf-8", "replace")
+        else:
+            message = str(value)
+        super().__init__(message)
+
+
 class LuaQuotaError(LuaRuntimeError):
     pass
